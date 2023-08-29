1c3fd5779f668ab9c07970232f2cc489fe9d1177
diana080299 <132782216+diana080299@users.noreply.github.com>
2023-08-29 19:02:24 +0100

    add

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const titleEl = document.querySelector("h2");
+console.log(titleEl);
+console.dir(titleEl);
+console.log(titleEl.textContent);
+const itemEl = document.querySelector("#categories");
+console.log(itemEl);
+console.dir(itemEl);
+console.log(itemEl.textContent);
